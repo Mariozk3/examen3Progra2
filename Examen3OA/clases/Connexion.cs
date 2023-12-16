5cc645c0458972255d0ba9fa0d7a3ad8e003f587
Mariozk3 <oscarmarioavila3@gmail.com>
2023-12-15 19:45:44 -0600

    Agregar archivos de proyecto.

---
+++ Examen3OA/clases/Connexion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+//Tipo de conexion:
+namespace Examen_Progra2:.Classes
+{   
+    public class Connexion
+    {
+        public static SqlConnection getConnection()
+        {
+            string cnxStr = ConfigurationManager.ConnectionStrings["cnxExamen"].ConnectionString;
+            SqlConnection cnx = new SqlConnection(cnxStr);
+            cnx.Open();
+
+            return cnx;
+        }
+
+        public static void closeConnection(SqlConnection cnx)
+        {
+            cnx.Close();
+        }
+    }
+}
